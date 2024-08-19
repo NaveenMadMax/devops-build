56053584ad970fa81a5ba5f52fa0bb27623f5800
Naveen <naveennavi2000@gmail.com>
2024-08-20 00:18:05 +0530

    Initial commit with Dockerization

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Build Docker image
+docker build -t yourdockerhubusername/devops-build:latest .
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run Docker container
+docker run -d -p 80:80 yourdockerhubusername/devops-build:latest
